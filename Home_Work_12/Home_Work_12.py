a72cce4e2f266b27c5b2373972c67e294d8b67b6
Тимур Ибатулин <timuribatulin@iMac-Timur-2.Dlink>
2023-09-14 14:02:37 +0300

    второй коммит

--- Home_Work_12/Home_Work_12.py
+++ Home_Work_12/Home_Work_12.py
@@ -4,6 +4,9 @@
 # - Для каждого предмета можно хранить оценки (от 2 до 5) и результаты тестов (от 0 до 100). 
 # - Также экземпляр должен сообщать средний балл по тестам для каждого предмета и по оценкам всех предметов вместе взятых.
 
+import csv
+
+
 class Examination:
     def __init__(self):
         self.__sum_name = None
@@ -13,7 +16,6 @@ class Examination:
     
     def __set__(self, instance, value):
         for i in value:
-            print(i)
             if i.isdigit():
                 raise ValueError("ОШИБКА! ФИО не может содержать число")
         if not str(value).istitle():
@@ -23,24 +25,34 @@ class Examination:
         def __delete__(self, instance):
             del self.__sum_name
         
-    
+def scoll_optimization(lessons, scool_class):
+    with open(f'scool_{lessons}{scool_class}.csv', 'r', newline = '') as f:
+        csv_file = f.read().split('\n')
+        print(f'{csv_file = }')
+        lessons ={}.fromkeys(csv_file, [])
+    return lessons
 
 
 class StudentLiteLifi:
     _surname = Examination()
     _name = Examination()
     _secondname = Examination()
-    def __init__(self, surname, name, secondname):
+    def __init__(self, surname, name, secondname, scool_class):
         self._surname = surname
         self._name = name
         self._secondname = secondname
-    
+        self._lessons = scoll_optimization('lessons', scool_class)
+        self._test = scoll_optimization('test', scool_class)
+
+    # def 
+
+
     def __str__(self) -> str:
-            return f'фамилия - {self._surname}\nимя - {self._name}\nотчество - {self._secondname}'
+            return f'фамилия - {self._surname}\nимя - {self._name}\nотчество - {self._secondname}\n{self._lessons}\n{self._test}'
 
 
 if __name__ == '__main__':
-    w = StudentLiteLifi('Ибат5улин', 'Тимур', 'Рамилевич')
+    w = StudentLiteLifi('Ибатулин', 'Тимур', 'Рамилевич', 1)
 
     print(w)
         
